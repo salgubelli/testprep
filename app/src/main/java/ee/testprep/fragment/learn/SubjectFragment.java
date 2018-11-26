4a150b5d0ad631d48c8bbbb47a9fa5f0de3e1f60
Siva Penke <>
2018-11-25 21:05:50 -0800

    testprep_ee: update new fragment files for learn fragment
    Signed-off-by: Siva Penke <>

---
+++ app/src/main/java/ee/testprep/fragment/learn/SubjectFragment.java
@@ -0,0 +1,137 @@
+package ee.testprep.fragment.learn;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import ee.testprep.R;
+
+public class SubjectFragment extends Fragment {
+
+    private static String className = SubjectFragment.class.getSimpleName();
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private ArrayList<String> subjects;
+
+    private SubjectFragment.OnFragmentInteractionListener mListener;
+
+    public SubjectFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param subjects Parameter 2.
+     * @return A new instance of fragment SubjectFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SubjectFragment newInstance(String param1, ArrayList<String> subjects) {
+        SubjectFragment fragment = new SubjectFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putSerializable(ARG_PARAM2, subjects);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            subjects = (ArrayList<String>) getArguments().getSerializable(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_subject, container, false);
+
+        final ListView listview = view.findViewById(R.id.lv_subject);
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(container.getContext(), R.layout.listview_item, R.id.icon, subjects);
+
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+/*                                list.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);*/
+                            }
+                        });
+            }
+
+        });
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
